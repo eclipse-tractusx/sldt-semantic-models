be0f47fa202736d32266bfd635af7a5887b3a3a1
Sahil Aggarwal <136346074+agg3fe@users.noreply.github.com>
2024-02-05 21:50:16 +0100

    Update index.js

--- .github/actions/validate-single/index.js
+++ .github/actions/validate-single/index.js
@@ -25,7 +25,7 @@ var output = {
 try {
     const model = JSON.parse(getInput('ADDED'))
     console.log("model:"+model)
-    deploySingleAspect(model+".ttl","release")
+    deploySingleAspect("io.catenax.generic.digital_product_passport/3.0.0/DigitalProductPassport.ttl","release")
     setOutput()
 
 } catch (error) {
