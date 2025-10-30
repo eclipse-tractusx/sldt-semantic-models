e434abedc8097619f61e6e1a56bd18a7d03bb108
Johann Schütz <147107265+jSchuetz88@users.noreply.github.com>
2025-10-30 22:25:26 +0100

    Add copyright and license information
    Added copyright and licensing information to the script.

--- .github/scripts/check-model-states.py
+++ .github/scripts/check-model-states.py
@@ -1,5 +1,19 @@
-import os
+#######################################################################
+# Copyright (c) 2025 Catena-X Automotive Network e.V.
+# Copyright (c) 2025 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This work is made available under the terms of the
+# Creative Commons Attribution 4.0 International (CC-BY-4.0) license,
+# which is available at
+# https://creativecommons.org/licenses/by/4.0/legalcode.
+#
+# SPDX-License-Identifier: CC-BY-4.0
+#######################################################################
 
+import os
 
 import re
 import json
